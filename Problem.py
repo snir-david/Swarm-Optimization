523595c00b91348cb2dbcfd1cab64b817a248d5d
snir-david <snirdavid1994@gmail.com>
2022-08-11 22:02:31 +0300

    adding comments and compare function

--- Problem.py
+++ Problem.py
@@ -1,22 +1,12 @@
-import random
-
 import numpy as np
 
 
-class Function:
-    def __init__(self, dim: int, type: bool):
-        self.dim = dim
-        self.type = type
-
-    def solution_fitness(self, x, y):
-        return 5000 - 10 * x + 40 * y + x * y - 0.8 * (y ** 2) - 0.5 * (x ** 2)
-
-
 class Problem:
-    def __init__(self, dim, min_val, max_val):
+    def __init__(self, dim, min_val, max_val, is_min_prob: bool = False):
         self.dim = dim
         self.min_val = min_val
         self.max_val = max_val
+        self.is_min_prob = is_min_prob
 
     def solution_fitness(self, solution):
         pass
@@ -25,10 +15,10 @@ class Problem:
         pass
 
 
-class Func1(Problem):
+class FunctionOptimization(Problem):
 
-    def __init__(self, dim, min_val, max_val):
-        super().__init__(dim, min_val, max_val)
+    def __init__(self, dim, min_val, max_val, is_min_prob: bool = False):
+        super().__init__(dim, min_val, max_val, is_min_prob)
 
     def solution_fitness(self, solution):
         return 5000 - 10 * solution[0] + 40 * solution[1] + solution[0] * solution[1] \
@@ -36,3 +26,5 @@ class Func1(Problem):
 
     def init_range(self):
         return np.random.uniform(0, 100, self.dim)
+
+
--- Swarm.py
+++ Swarm.py
@@ -12,53 +12,70 @@ class Particle:
 
 
 class Swarm:
-    def __init__(self, size, w, c1, c2, problem, is_min_prob=True, need_init: bool = True):
+    def __init__(self, size, w, c1, c2, problem, need_init: bool = True):
         self.swarm_size = size
         self.swarm = []
+        # global best
         self.global_best_fitness = 0
         self.global_best_position = None
         self.global_best_particle = None
+        # Hyper-Parameters
         self.w = w
         self.c1 = c1
         self.c2 = c2
-        self.is_min_prob = is_min_prob
+        # Problem
         self.problem = problem
         if need_init:
             self.init_swarm()
 
     def init_swarm(self):
         for i in range(self.swarm_size):
+            # initialize particle
             p = Particle(self, self.problem.init_range)
+            # calc fitness
             p.fitness = self.problem.solution_fitness(p.position)
             self.swarm.append(p)
-            if p.fitness > self.global_best_fitness:
+            # compare current particle fitness with global best
+            if self.compare_fitness(p.fitness, self.global_best_fitness):
                 self.set_global_best(p)
 
     def new_velocity(self, p: Particle):
+        # random variables
         r1 = np.random.uniform(0, 1)
         r2 = np.random.uniform(0, 1)
+        # calc new velocity using the equation - v(t+1)= w * v(t) + c1 * r1 * (local_best - x(t))
+        # + c2 * r2 * (global_best - x(t))
         v_t_1 = self.w * p.velocity + self.c1 * r1 * (p.local_best_position - p.position) \
                 + self.c2 * r2 * (self.global_best_position - p.position)
         p.velocity = v_t_1
 
     def new_position(self, p: Particle):
+        # calc new velocity
         self.new_velocity(p)
+        # calc new position
         x_t_1 = p.position + p.velocity
         p.position = x_t_1
+        # calc new fitness and check it against the local and global
         p.fitness = self.problem.solution_fitness(p.position)
-        if p.fitness > self.global_best_fitness:
-            self.global_best_fitness = p.fitness
-            self.global_best_position = p.position
-        if p.fitness > p.local_best_fitness:
+        if self.compare_fitness(p.fitness, self.global_best_fitness):
+            self.set_global_best(p)
+        if self.compare_fitness(p.fitness, p.local_best_fitness):
             p.local_best_fitness = p.fitness
             p.local_best_position = p.position
 
-    def local_best_update(self, p: Particle):
-        if self.is_min_prob:
-            pass
+    def compare_fitness(self, f1: int, f2: int):
+        # check if the problem is min or max and compare accordingly
+        # min problem
+        if self.problem.is_min_prob:
+            if f1 < f2:
+                return True
+            return False
+        else: # max problem
+            if f1 > f2:
+                return True
+            return False
 
     def set_global_best(self, p: Particle):
         self.global_best_fitness = p.fitness
         self.global_best_position = p.position
         self.global_best_particle = p
-
